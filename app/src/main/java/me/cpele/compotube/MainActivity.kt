5d06c9fa39011f58da5d1b0b1caa14159b3cc6d5
geekarist <geekarist@users.noreply.github.com>
2022-08-08 09:31:50 +0200

    Fix Gradle build error

--- app/src/main/java/me/cpele/compotube/MainActivity.kt
+++ app/src/main/java/me/cpele/compotube/MainActivity.kt
@@ -16,8 +16,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import kotlinx.parcelize.Parcelize
 import me.cpele.compotube.ui.theme.CompotubeTheme
@@ -26,7 +29,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            rememberCoroutineScope()
             CompotubeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
