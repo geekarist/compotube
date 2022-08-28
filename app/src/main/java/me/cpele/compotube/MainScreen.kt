dfa69c6ee1ee04a8b36af358fe0ebd80e934cc43
geekarist <geekarist@users.noreply.github.com>
2022-08-28 15:51:25 +0200

    Add login button and "account chosen" effect

--- app/src/main/java/me/cpele/compotube/MainScreen.kt
+++ app/src/main/java/me/cpele/compotube/MainScreen.kt
@@ -6,12 +6,17 @@ package me.cpele.compotube
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.util.ExponentialBackOff
+import com.google.api.services.youtube.YouTubeScopes
 import me.cpele.compotube.kits.Main
 import me.cpele.compotube.mvu.Effect
 
@@ -41,4 +46,20 @@ private fun log(text: String) {
 
 private fun toast(context: Context, text: String) {
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+private fun ChooseAccount(dispatch: (Main.Event) -> Unit) {
+    val contract = ActivityResultContracts.StartActivityForResult()
+    val launcher = rememberLauncherForActivityResult(contract = contract) { result ->
+        dispatch(Main.Event.AccountChosen(result))
+    }
+    val context = LocalContext.current.applicationContext
+    val chooseAccountIntent = rememberSaveable {
+        val scopes = listOf(YouTubeScopes.YOUTUBE_READONLY)
+        val backOff = ExponentialBackOff()
+        val credential = GoogleAccountCredential.usingOAuth2(context, scopes).setBackOff(backOff)
+        credential.newChooseAccountIntent()
+    }
+    launcher.launch(chooseAccountIntent)
 }
--- app/src/main/java/me/cpele/compotube/kits/Main.kt
+++ app/src/main/java/me/cpele/compotube/kits/Main.kt
@@ -4,6 +4,7 @@ package me.cpele.compotube.kits
 
 import android.os.Parcelable
 import android.view.KeyEvent
+import androidx.activity.result.ActivityResult
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,10 +12,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.text.input.ImeAction
@@ -26,54 +29,73 @@ import me.cpele.compotube.mvu.Effect
 object Main {
 
     @Parcelize
-    data class Model(val query: String = "") : Parcelable
+    data class Model(val isLoggedIn: Boolean = false, val query: String = "") : Parcelable
 
     @Composable
     fun View(model: Model, dispatch: (Event) -> Unit) {
-        Column(modifier = Modifier.focusable(false)) {
-            Box(
-                Modifier
-                    .wrapContentHeight()
-            ) {
-                TextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .focusableWithArrowKeys()
-                        .onKeyEvent { event ->
-                            if (event.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
-                                dispatch(Event.QuerySent)
-                                true
-                            } else {
-                                false
-                            }
+        if (model.isLoggedIn) {
+            Column(modifier = Modifier.focusable(false)) {
+                Box(
+                    Modifier
+                        .wrapContentHeight()
+                ) {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .focusableWithArrowKeys()
+                            .onKeyEvent { event ->
+                                if (event.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                                    dispatch(Event.QuerySent)
+                                    true
+                                } else {
+                                    false
+                                }
+                            },
+                        singleLine = true,
+                        value = model.query,
+                        placeholder = { Text("Search Compotube") },
+                        onValueChange = { value ->
+                            dispatch(Event.QueryChanged(value))
                         },
-                    singleLine = true,
-                    value = model.query,
-                    placeholder = { Text("Search Compotube") },
-                    onValueChange = { value ->
-                        dispatch(Event.QueryChanged(value))
-                    },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                    keyboardActions = KeyboardActions(onSearch = {
-                        dispatch(Event.QuerySent)
-                    })
-                )
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(onSearch = {
+                            dispatch(Event.QuerySent)
+                        })
+                    )
+                }
+            }
+        } else {
+            Box {
+                Button(
+                    modifier = Modifier.align(Alignment.Center),
+                    onClick = { dispatch(Event.LoginRequested) }
+                ) {
+                    Text("Login")
+                }
             }
         }
     }
 
     sealed class Event {
         object QuerySent : Event()
+        object LoginRequested : Event()
         data class QueryChanged(val value: String) : Event()
+        data class AccountChosen(val result: ActivityResult) : Event()
     }
 
     fun update(model: Model, event: Event): Change<Model> =
         when (event) {
-            is Event.QueryChanged -> Change(
-                model.copy(query = event.value),
-                Effect.Log("You're looking for ${event.value}")
-            )
-            is Event.QuerySent -> Change(model, Effect.Toast("Query sent: ${model.query}"))
+            is Event.QueryChanged ->
+                Change(
+                    model.copy(query = event.value),
+                    Effect.Log("You're looking for ${event.value}")
+                )
+            is Event.QuerySent ->
+                Change(model, Effect.Toast("Query sent: ${model.query}"))
+            is Event.LoginRequested ->
+                Change(model, Effect.Toast("Login requested (TODO)"))
+            is Event.AccountChosen ->
+                Change(model, Effect.Log("Account chosen: ${event.result}"))
         }
 }
 
