49cc1f14a41527558caea78aff4bc415c7082617
geekarist <geekarist@users.noreply.github.com>
2022-09-04 15:57:59 +0200

    Handle dispose event immediately on dispose

--- app/src/main/java/me/cpele/compotube/MainScreen.kt
+++ app/src/main/java/me/cpele/compotube/MainScreen.kt
@@ -34,38 +34,63 @@ fun MainScreen() {
     val context = LocalContext.current.applicationContext
     val lifecycleOwner = LocalLifecycleOwner.current
 
-    LaunchedEffect(Unit) { // When opening the screen, collect events
+    LaunchedEffect(Unit) {
 
+        // Setup model init/dispose
         lifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-
             override fun onCreate(owner: LifecycleOwner) {
                 super.onCreate(owner)
                 eventFlow.value = Main.Event.Init
             }
 
             override fun onDestroy(owner: LifecycleOwner) {
-                eventFlow.value = Main.Event.Dispose
+                // The event must be handled immediately because on destroy,
+                // it won't be collected from the Flow
+                handleEvent(
+                    context,
+                    model,
+                    event = Main.Event.Dispose,
+                    onNewModel = {}, // Won't change the model
+                    launchIntent = {}, // Won't launch intent
+                    dispatch = {}) // Won't dispatch
                 super.onDestroy(owner)
             }
         })
 
+        // Start collecting events
         eventFlow.filterNotNull().collect { event ->
-            val change = Main.update(model, event)
-            model = change.model
-            change.effects.forEach { effect ->
-                execute(
-                    context,
-                    effect,
-                    launch = { launcher.launch(it) },
-                    dispatch = { eventFlow.value = it }
-                )
-            }
+            handleEvent(
+                context, model, event,
+                onNewModel = { newModel: Main.Model -> model = newModel },
+                launchIntent = { launcher.launch(it) },
+                dispatch = { eventFlow.value = it }
+            )
         }
     }
 
     Main.View(model = model, dispatch = { event ->
         eventFlow.value = event
     })
+}
+
+private fun handleEvent(
+    context: Context,
+    model: Main.Model,
+    event: Main.Event,
+    onNewModel: (Main.Model) -> Unit,
+    launchIntent: (Intent) -> Unit,
+    dispatch: (Main.Event) -> Unit
+) {
+    val change = Main.update(model, event)
+    onNewModel(change.model)
+    change.effects.forEach { effect ->
+        execute(
+            context,
+            effect,
+            launch = launchIntent,
+            dispatch = dispatch
+        )
+    }
 }
 
 private fun execute(
@@ -82,7 +107,7 @@ private fun execute(
             onPrefLoaded = { dispatch(Main.Event.StrPrefLoaded(it)) }
         )
         is Effect.Toast -> toast(context, effect.text)
-        is Effect.Log -> log(effect.text, effect.throwable)
+        is Effect.Log -> log(effect.tag, effect.text, effect.throwable)
         Effect.GetAppContext -> dispatch(Main.Event.AppContextReceived(context.applicationContext))
         is Effect.ActForResult -> launch(effect.intent)
         is Effect.SavePref -> saveStrPref(
@@ -94,7 +119,7 @@ private fun execute(
 }
 
 fun saveStrPref(context: Context, name: String, value: String) {
-    Log.d("", "TODO: Save pref")
+    Log.d("chrp", "TODO: Save pref")
 }
 
 fun loadStrPref(
@@ -103,11 +128,11 @@ fun loadStrPref(
     defValue: String?,
     onPrefLoaded: (String?) -> Unit
 ) {
-    Log.d("", "TODO: Load pref")
+    Log.d("chrp", "TODO: load pref")
 }
 
-private fun log(text: String, throwable: Throwable?) {
-    Log.d("", text, throwable)
+private fun log(tag: String, text: String, throwable: Throwable?) {
+    Log.d(tag, text, throwable)
 }
 
 private fun toast(context: Context, text: String) {
